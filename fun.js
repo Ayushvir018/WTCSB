827722c4a5b8f58220414f0faae3b2995298b68e
Ayush Gupta <ag7654641@gmail.com>
2026-01-29 11:09:52 +0530

    29-01-2026

--- fun.js
+++ fun.js
@@ -20,10 +20,29 @@
 // }
 // new per();
 //"5"
-function per(){
-    this.age=25;
-    setTimeout(()=>{
-        console.log(this.age);
-    },1000);
-}
-new per();
+// function per(){
+//     this.age=25;
+//     setTimeout(()=>{
+//         console.log(this.age);
+//     },1000);
+// }
+// new per();
+// 6
+const aa= ()=> { return { user: "name"}};
+console.log(aa());
+const bb= ()=> [1,2,3,4,5];
+console.log(bb());
+
+const user = {
+    name : "ag", age :20,
+    welcome :  function(){
+        console.log(`hello ${this.name}`);
+        console.log(this)
+
+    }
+};
+const f=user.welcome();
+console.log(f);
+
+// user.name= "ayush";
+// user.welcome();
